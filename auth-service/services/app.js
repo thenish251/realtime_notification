9ee42587c44d440c4a29f63c1071b1da4b762d5e
thenish51 <danish@sosimtech.com>
2024-07-06 15:54:41 +0530

    services-updated

--- auth-service/services/app.js
+++ auth-service/services/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');
+const authRoutes = require('../routes/auth');
 
 dotenv.config();
 
